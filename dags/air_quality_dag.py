0017cd8e1d661cc35c4d49037f1fd8e27d910020
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2025-01-24 12:44:16 -0300

    :sparkles: feat: Criação da task para camada diamond

--- dags/air_quality_dag.py
+++ dags/air_quality_dag.py
@@ -161,7 +161,7 @@ def push_to_s3_gold():
     conn.execute("ALTER TABLE pollutants_gold DROP COLUMN data")
     
     
-    conn.execute(f"""
+    conn.execute("""
         COPY (
             SELECT *
             FROM pollutants_gold
@@ -172,6 +172,126 @@ def push_to_s3_gold():
 
     hook.load_file(filename='./data/gold/pollutants_data.parquet', key=f'pollutants_data_{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.parquet', bucket_name=s3)
 
+def push_to_s3_diamond():
+
+    s3 = "desafio4-diamond"
+
+    hook = S3Hook('aws-teste', region_name='sa-east-1')
+    
+    conn = duckdb.connect()
+
+    conn.execute("""
+        CREATE TABLE pollutants_data AS SELECT * FROM './data/gold/pollutants_data.parquet'        
+    """)
+
+    conn.execute("""CREATE TABLE pollutants_diamond(
+        code_id INT,     
+        values NUMERIC(5,2),
+        units_id INT,
+        year_id INT,
+        month_id int,
+        day_id INT,
+        time TIME             
+    )""")
+
+    conn.execute("""
+    CREATE TABLE codes AS
+    SELECT ROW_NUMBER() OVER () AS id, code
+    FROM (SELECT DISTINCT code FROM pollutants_data);
+""")
+    
+    conn.execute("""
+    CREATE TABLE years AS
+    SELECT ROW_NUMBER() OVER () AS id, year
+    FROM (SELECT DISTINCT year FROM pollutants_data);
+""")
+    
+    conn.execute("""
+    CREATE TABLE months AS
+    SELECT ROW_NUMBER() OVER () AS id, month
+    FROM (SELECT DISTINCT month FROM pollutants_data);
+""")
+    
+    conn.execute("""
+    CREATE TABLE days AS
+    SELECT ROW_NUMBER() OVER () AS id, day
+    FROM (SELECT DISTINCT day FROM pollutants_data);
+""")
+    
+    conn.execute("""
+    CREATE TABLE units AS
+    SELECT ROW_NUMBER() OVER () AS id, units
+    FROM (SELECT DISTINCT units FROM pollutants_data);
+""")
+    
+    conn.execute("""
+    INSERT INTO pollutants_diamond (code_id, values, units_id, year_id, month_id, day_id, time)
+    SELECT
+        (SELECT id FROM codes WHERE code = pollutants_data.code),
+        values,
+        (SELECT id FROM units WHERE units = pollutants_data.units),
+        (SELECT id FROM years WHERE year = pollutants_data.year),
+        (SELECT id FROM months WHERE month = pollutants_data.month),
+        (SELECT id FROM days WHERE day = pollutants_data.day),
+        pollutants_data.time
+    FROM pollutants_data;    
+""")
+    
+    conn.execute("""
+        COPY (
+            SELECT *
+            FROM pollutants_diamond
+        ) TO './data/diamond/pollutants_data.parquet' (FORMAT PARQUET);
+    """)
+
+    conn.execute("""
+        COPY (
+            SELECT *
+            FROM years
+        ) TO './data/diamond/years.parquet' (FORMAT PARQUET);
+    """)
+
+    conn.execute("""
+        COPY (
+            SELECT *
+            FROM months
+        ) TO './data/diamond/months.parquet' (FORMAT PARQUET);
+    """)
+
+    conn.execute("""
+        COPY (
+            SELECT *
+            FROM days
+        ) TO './data/diamond/days.parquet' (FORMAT PARQUET);
+    """)
+
+    conn.execute("""
+        COPY (
+            SELECT *
+            FROM codes
+        ) TO './data/diamond/codes.parquet' (FORMAT PARQUET);
+    """)
+
+    conn.execute("""
+        COPY (
+            SELECT *
+            FROM units
+        ) TO './data/diamond/units.parquet' (FORMAT PARQUET);
+    """)
+
+    conn.close()
+
+    hook.load_file(filename='./data/diamond/pollutants_data.parquet', key=f'pollutants_data_{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.parquet', bucket_name=s3)
+
+    hook.load_file(filename='./data/diamond/codes.parquet', key=f'codes_{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.parquet', bucket_name=s3)
+
+    hook.load_file(filename='./data/diamond/years.parquet', key=f'years_{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.parquet', bucket_name=s3)
+
+    hook.load_file(filename='./data/diamond/months.parquet', key=f'months_{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.parquet', bucket_name=s3)
+
+    hook.load_file(filename='./data/diamond/days.parquet', key=f'days_{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.parquet', bucket_name=s3)
+
+    hook.load_file(filename='./data/diamond/units.parquet', key=f'units_{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.parquet', bucket_name=s3)
 
 
 with DAG(
@@ -227,4 +347,9 @@ with DAG(
         python_callable=push_to_s3_gold
     )
 
-    task_fetch >> task_generate_data >> [task_s3_raw, task_s3_silver, task_s3_gold]
+    task_s3_diamond = PythonOperator(
+        task_id='push_to_s3_diamond',
+        python_callable=push_to_s3_diamond
+    )
+
+    task_fetch >> task_generate_data >> [task_s3_raw, task_s3_silver, task_s3_gold, task_s3_diamond]
