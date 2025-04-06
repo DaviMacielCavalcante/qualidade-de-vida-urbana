22c47a0bf4f42fe5898e629adef358377e793ce6
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2025-04-06 13:14:12 -0300

    :recycle: refactor: ajustes nos scripts sql

--- dags/SQL/DDL/google/create_tables_bronze.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS bronze.google_api_data(
-	id SERIAL PRIMARY KEY,
-	data JSONB
-);
--- dags/SQL/DDL/weather/create_tables_bronze.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS bronze.weather_api_data(
-	id SERIAL PRIMARY KEY,
-	data JSONB
-);
