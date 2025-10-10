230fbf9436f899b2151012d2bf072ba351aa9f35
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2025-10-10 14:24:02 -0300

    🔨 chore: Atualizando o schema do usuário e registrando a adição de uma lib

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -6,10 +6,25 @@ class UserSchema(BaseModel):
     id: str
     name: str = Field(..., min_length=1, max_length=100)
     email: EmailStr = Field(...)
-    password: str = Field(..., min_length=8, max_length=255)
     signatureStatus: UserSignatureStatusEnum | None
     phone_number: str = Field(..., min_length=11, max_length=20, alias="phoneNumber")
     role: UserRoleEnum | None
     notifications: UserNotificationPreferenceEnum | None
+class UserCreate(UserSchema):
+    password: str = Field(..., min_length=8, max_length=255)
+
+class UserRead(UserSchema):
     created_at: datetime = Field(..., alias="createdAt")
     updated_at: datetime = Field(..., alias="updatedAt") | None
+
+    class Config: 
+        orm_mode = True
+
+class UserUpdate(UserSchema):
+    name: str | None = None
+    email: EmailStr | None = None
+    phone_number: str | None = Field(None, min_length=11, max_length=20, alias="phoneNumber")
+    password: str | None = Field(None, min_length=8, max_length=255)
+    signatureStatus: UserSignatureStatusEnum | None = None
+    role: UserRoleEnum | None = None
+    notifications: UserNotificationPreferenceEnum | None = None
