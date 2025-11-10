2a78b5b53fda5a26651f80a3c7e5e58a344203ed
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2025-11-10 09:14:05 -0300

    ✨ feat: criação da rota para cadastro e login automático, e rota para usuário se cadastrar e já ficar logado

--- app/auth/auth_router.py
+++ app/auth/auth_router.py
@@ -2,11 +2,22 @@ from fastapi import APIRouter, Depends, HTTPException, status, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
+from .dependencies import get_current_user
+from .jwt_handler import create_access_token
 from ..database import get_db
+from ..schemas.user_schema import UserRead
 from ..use_cases.auth_use_case import LoginUseCase
+from ..use_cases.user_use_case import User, UserCreate, CreateUserUseCase
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
+@router.get("/me")
+def myself(
+    current_user: User = Depends(get_current_user)
+) -> UserRead:
+   
+    return current_user
+   
 @router.post("/login")
 def login(
     response: Response,
@@ -37,10 +48,36 @@ def login(
     
 
 @router.post("/logout")
-def logou(response: Response):
+def logout(response: Response):
     response.delete_cookie(key="access_token")
     return {"message": "Logout successful"}
     
-    
 
+@router.post("/register")
+def register(
+    response: Response,
+    user_data: UserCreate,
+    db: Session = Depends(get_db)
+) -> UserRead:
     
+    try:
+        user = CreateUserUseCase.execute(db=db, user_create=user_data)
+    except ValueError as e:
+        if "already used" in str(e):
+            raise HTTPException(status_code=409, detail=str(e))
+        
+        raise HTTPException(status_code=400, detail=str(e))
+
+    token = create_access_token(data={"sub": str(user.id)})
+
+
+    response.set_cookie(
+            key="access_token",
+            value=token,
+            httponly=True,
+            max_age=900,
+            secure=False,
+            samesite="lax"
+        )
+    
+    return user
