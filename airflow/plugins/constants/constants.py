884372fcb00bc11bd0dd0c4ca1efcc2774a1d303
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2026-02-03 13:29:00 -0300

    🚧  chore: adicionando novas constantes para a dag do google

--- airflow/plugins/constants/constants.py
+++ airflow/plugins/constants/constants.py
@@ -2,3 +2,6 @@ AIRFLOW_AWS_S3_SECRET_KEY_ID_PATH = "/tcc/dev/airflow_aws_s3_key_id"
 AIRFLOW_AWS_S3_SECRET_PATH = "/tcc/dev/airflow_s3_secret"
 S3_REGION_PATH = "/tcc/dev/aws_region"
 S3_BUCKET_BRONZE_PATH = "/tcc/dev/aws_s3_bucket_bronze"
+AIRFLOW_GOOGLE_WEATHER_API_KEY_PATH = "/tcc/dev/google_weather_api_key"
+CASTANHAL_LATITUDE = -1.2969
+CASTANHAL_LONGITUDE = -47.9219
